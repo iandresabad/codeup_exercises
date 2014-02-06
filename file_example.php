0881df3f9c6e223ece42f6330e73c350053a0bcf
iandresabad <iandresabad@yahoo.com>
2014-02-06 11:52:53 -0600

    completed exercise on if
    Code Up exercise for if.

---
+++ file_example.php
@@ -0,0 +1,45 @@
+<?php 
+
+$a = 5;
+$b = 10;
+$c = '10';
+
+if ($a < $b) {
+	//output the appropriate result
+	echo "$a is less than $b\n";
+}
+ 
+ if ($b > $a) {
+ 	// output the appropiate result 
+ 	echo "$b is greater than $a\n";
+ }
+
+ if ($b >= $c) {
+ 	// output the appropriate result 
+ 	echo "$b is greater than or equal to $c\n";
+ }
+if ($b <= $c) {
+	// ouptput the appropiate resut 
+	echo "$b is less than or equal to $c\n";
+}
+if ($b == $c) {
+	// output the appropiate result 
+	echo "$b is equal to $c\n";
+}
+
+if ($b === $c) {
+	// output the appropriate result 
+	echo "$b is equal or different than $c\n";
+}
+
+if ($b != $c) {
+	// output the appropiate result 
+	echo "$b is not equal to $c\n";
+}
+
+//output the appropiate result 
+if ($b !== $c) {
+echo "$b is not identtical to $c\n";
+}
+
+?> 
