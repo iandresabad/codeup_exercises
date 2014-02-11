8cd20ce01f00968fb21a0febf87ca01cc1e843b5
iandresabad <iandresabad@yahoo.com>
2014-02-11 11:24:35 -0600

    simple math problems

---
+++ arithmetic.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+function add($a, $b) {
+    echo ($a + $b) . "\n";
+}
+
+function subtract($a, $b) {
+    // Add code here
+    echo ($a - $b) . "\n";
+}
+
+function multiply($a, $b) {
+    // Add code here
+    echo ($a * $b) . "\n" ;
+}
+
+function divide($a, $b) {
+    // Add code here
+    echo ($a / $b) . "\n";
+}
+function modulus($a, $b) {
+	echo ($a % $b) . "\n";
+}
+
+$a = 30;
+$b = 10;
+
+add($a, $b);
+
+
+add(69, 7);
+subtract(50,6);
+multiply(45,5);
+divide(100,5);
+modulus(5,50); 
+
+
+?>
