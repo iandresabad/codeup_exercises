666a26a0ee37fda00a77e9bcb2339955528aaa06
iandresabad <iandresabad@yahoo.com>
2014-02-10 18:55:08 -0600

    counting in even, and then stopping at 10

---
+++ breakcontinue.php
@@ -0,0 +1,16 @@
+<?php
+
+for ($i = 1; $i <= 100; $i++) {
+	if ($i % 2 !== 0) {
+		continue;
+	}
+		echo $i . "\n";
+}
+for ($i = 1; $i <= 100; $i++) {
+	if ($i > 10) {
+		break;
+	}
+		echo $i . "\n";
+}
+
+?>
